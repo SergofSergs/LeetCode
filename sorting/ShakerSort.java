3b6710f168064f3340b9f5ead3ad3769efac39a6
SergofSergs <sergeibobkov2010@gmail.com>
2022-02-22 13:57:57 +0300

    Shaker sort + some modifications to the test class

---
+++ sorting/ShakerSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+public class ShakerSort
+{
+	public static int[] shakerSort(int[] arr)
+	{
+		int len = arr.length - 1; //Last element of the array (End)
+		int ln;
+		int i = 0; // First element of the array (Start)
+
+		// Algorithm goes till the indexes meets in the middle
+		while (i < len)
+		{
+			ln = len;
+			for (int j = i ;j < len; j++)
+			/*In first loop goes through the array swapping elements in places
+			just like bubble sort */
+			{
+				if (arr[j] > arr[j + 1])
+					Swap.swap(j, j + 1, arr);
+				// Decided to write swap method, changes start and end elements in places
+			}
+			/*In second, it goes backwards, but does the same job*/
+			for (;ln > i; ln--)
+			{
+				if (arr[ln] < arr[ln - 1])
+					Swap.swap(ln, ln - 1, arr);
+			}
+			i++;
+			len--;
+		}
+		return arr;
+	}
+}
--- sorting/TestClass.java
+++ sorting/TestClass.java
@@ -4,27 +4,33 @@ import java.util.Arrays;
 
 public class TestClass
 {
-    @FunctionalInterface
-    interface TestedFunction
-    {
-        int[] Sort(int[] arr);
-    }
-    private static int[] getTest(TestedFunction ref, int[] arr)
-    {
-        return (ref.Sort(arr));
-    }
-    public static void main(String[] args)
-    {
-        int[][] arr = {{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-        {10, 10, 9, 9, 8, 8, 7, 6, 5, 4, 3, 2, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {10, 9, 8, 7, 6, 1, 2, 3, 4, 5},
-        {}, {1, 2, 3, 4, 3, 3, 5, 6, 7},
-        {5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5},
-        {10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10}
-        };
-        for (int[] x: arr)
-            System.out.println(Arrays.toString(getTest(BubbleSort::bubbleSort, x)));
-    }
+	@FunctionalInterface
+	interface TestedFunction
+	{
+		int[] Sort(int[] arr);
+	}
+	private static int[] getTest(TestedFunction ref, int[] arr)
+	{
+		return (ref.Sort(arr));
+	}
+	public static void main(String[] args)
+	{
+		int[][] arr = {{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+				{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				{10, 10, 9, 9, 8, 8, 7, 6, 5, 4, 3, 2, 1},
+				{1, 1, 1, 1, 1, 1, 1, 1, 1},
+				{10, 9, 8, 7, 6, 1, 2, 3, 4, 5},
+				{}, {1, 2, 3, 4, 3, 3, 5, 6, 7},
+				{5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5},
+				{10, 9, 8, 7, 6, 5, 6, 7, 8, 9, 10}
+		};
+		long runTime = System.nanoTime();
+		for (int[] x: arr)
+		{
+			long time = System.nanoTime();
+			System.out.print(Arrays.toString(getTest(ShakerSort::shakerSort, x)) + " completed in ");
+			System.out.println(System.nanoTime() - time);
+		}
+		System.out.println("Overall running time is " + (System.nanoTime() - runTime));
+	}
 }
