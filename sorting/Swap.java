31e93816f25ccb243526814ce0164022c924524d
SergofSergs <sergeibobkov2010@gmail.com>
2022-02-22 13:58:29 +0300

    Swap method

---
+++ sorting/Swap.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class Swap
+{
+	public static void swap(int start, int end, int[] arr)
+	{
+		int temp;
+
+		temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+	}
+}
