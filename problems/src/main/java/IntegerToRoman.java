292b385579bb3d8c3a205ba9f02295ee4eb9aa48
SergofSergs <sergeibobkov2010@gmail.com>
2022-04-28 20:40:48 +0300

    Integer to roman - optimized(?)

--- problems/src/main/java/IntegerToRoman.java
+++ problems/src/main/java/IntegerToRoman.java
@@ -1,8 +1,12 @@
 // Transforming int values to roman number system  (1 <= num <= 3999)
+
 public class IntegerToRoman {
 	public String intToRoman(int num) {
 		int nLen = digitNumber(num); // Calculating number's length
 		String roman = ""; // The number being build down there
+		String def = "M";
+		String mid;
+		String over;
 
 		if (nLen > 4) // This check redundant in this case
 			return null;
@@ -12,71 +16,42 @@ public class IntegerToRoman {
 			// For the M (1000) it's that simple
 			if (nLen == 4) {
 				while (n != 0) {
-					roman = roman.concat(Roman.ONE_THOUSAND.toString());
+					roman = roman.concat(def);
 					n--;
 				}
 				// Every other case is following the one same pattern
 				// I don't want to explain it, you can just google how this number system works
-			} else if (nLen == 3) {
-				while (n > 0) {
-					if (n == 9) {
-						roman = roman.concat(Roman.ONE_HUNDRED.toString());
-						roman = roman.concat(Roman.ONE_THOUSAND.toString());
-						break;
-					}
-					if (n == 4) {
-						roman = roman.concat(Roman.ONE_HUNDRED.toString());
-						roman = roman.concat(Roman.FIVE_HUNDREDS.toString());
-						break;
-					}
-					if (n >= 5) {
-						roman = roman.concat(Roman.FIVE_HUNDREDS.toString());
-						n -= 5;
-					}
-					if (n != 0) {
-						roman = roman.concat(Roman.ONE_HUNDRED.toString());
-					}
-					n--;
-				}
-			} else if (nLen == 2) {
-				while (n > 0) {
-					if (n == 9) {
-						roman = roman.concat(Roman.TEN.toString());
-						roman = roman.concat(Roman.ONE_HUNDRED.toString());
-						break;
-					}
-					if (n == 4) {
-						roman = roman.concat(Roman.TEN.toString());
-						roman = roman.concat(Roman.FIFTY.toString());
-						break;
-					}
-					if (n >= 5) {
-						roman = roman.concat(Roman.FIFTY.toString());
-						n -= 5;
-					}
-					if (n != 0) {
-						roman = roman.concat(Roman.TEN.toString());
-					}
-					n--;
-				}
 			} else {
+				if (nLen == 3) {
+					def = "C";
+					mid = "D";
+					over = "M";
+				} else if (nLen == 2) {
+					def = "X";
+					mid = "L";
+					over = "C";
+				} else {
+					def = "I";
+					mid = "V";
+					over = "X";
+				}
 				while (n > 0) {
 					if (n == 9) {
-						roman = roman.concat(Roman.ONE.toString());
-						roman = roman.concat(Roman.TEN.toString());
+						roman = roman.concat(def);
+						roman = roman.concat(over);
 						break;
 					}
 					if (n == 4) {
-						roman = roman.concat(Roman.ONE.toString());
-						roman = roman.concat(Roman.FIVE.toString());
+						roman = roman.concat(def);
+						roman = roman.concat(mid);
 						break;
 					}
 					if (n >= 5) {
-						roman = roman.concat(Roman.FIVE.toString());
+						roman = roman.concat(mid);
 						n -= 5;
 					}
 					if (n != 0) {
-						roman = roman.concat(Roman.ONE.toString());
+						roman = roman.concat(def);
 					}
 					n--;
 				}
@@ -95,39 +70,8 @@ public class IntegerToRoman {
 			i++;
 		return (i);
 	}
-
-	// Also redundant, just playing with enums
-	private enum Roman {
-		ONE,
-		FIVE,
-		TEN,
-		FIFTY,
-		ONE_HUNDRED,
-		FIVE_HUNDREDS,
-		ONE_THOUSAND;
-
-		@Override
-		public String toString() {
-			switch (this) {
-				case ONE:
-					return "I";
-				case FIVE:
-					return "V";
-				case TEN:
-					return "X";
-				case FIFTY:
-					return "L";
-				case ONE_HUNDRED:
-					return "C";
-				case FIVE_HUNDREDS:
-					return "D";
-				default:
-					return "M";
-			}
-		}
-	}
 }
 /*
-	Runtime: 7 ms, faster than 72.53% of Java online submissions for Integer to Roman.
-	Memory Usage: 47 MB, less than 17.62% of Java online submissions for Integer to Roman.
+	Runtime: 6 ms, faster than 82.04% of Java online submissions for Integer to Roman.
+	Memory Usage: 43.2 MB, less than 74.90% of Java online submissions for Integer to Roman.
 */
